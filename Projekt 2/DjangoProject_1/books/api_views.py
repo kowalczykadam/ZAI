4d2b20457e2c601e3c2f2c0bbf480f5c3c2e8099
kowalczykadam <60419569+kowalczykadam@users.noreply.github.com>
2026-04-16 12:00:26 +0200

    Update api_views.py

--- Projekt 2/DjangoProject_1/books/api_views.py
+++ Projekt 2/DjangoProject_1/books/api_views.py
@@ -1,7 +1,25 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Book
 from .serializers import BookSerializer
+
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+    # filtrowanie po wielu polach
+    filter_backends = [
+        DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter
+    ]
+
+    filterset_fields = ['author', 'year']
+
+    # wyszukiwanie tekstowe
+    search_fields = ['title', 'author']
+
+    # sortowanie
+    ordering_fields = ['title', 'year']
+    ordering = ['title']
