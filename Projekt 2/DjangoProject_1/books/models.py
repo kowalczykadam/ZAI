500ad640865f00679a969246bed9dee923295e48
kowalczykadam <60419569+kowalczykadam@users.noreply.github.com>
2026-04-22 15:44:17 +0200

    Update models.py

--- Projekt 2/DjangoProject_1/books/models.py
+++ Projekt 2/DjangoProject_1/books/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 class Book(models.Model):
@@ -5,6 +6,12 @@ class Book(models.Model):
     author = models.CharField(max_length=200)
     year = models.IntegerField(null=True, blank=True)
     external_id = models.CharField(max_length=100, blank=True, null=True)
+
+    owner = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="books"
+    )
 
     def __str__(self):
         return self.title
