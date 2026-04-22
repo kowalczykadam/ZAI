3254ea48175b7ebe35bbfec284ba6442c0d94557
kowalczykadam <60419569+kowalczykadam@users.noreply.github.com>
2026-04-22 15:44:29 +0200

    Update serializers.py

--- Projekt 2/DjangoProject_1/books/serializers.py
+++ Projekt 2/DjangoProject_1/books/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
 from .models import Book
 
+
 class BookSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
     class Meta:
         model = Book
-        fields = '__all__'
+        fields = ['id', 'title', 'author', 'year', 'owner']
