202c4816d5d5dcbe2e80a9ae433565c0f0843f1f
kowalczykadam <60419569+kowalczykadam@users.noreply.github.com>
2026-04-22 15:45:09 +0200

    Update urls.py

--- Projekt 2/DjangoProject_1/DjangoProject_1/urls.py
+++ Projekt 2/DjangoProject_1/DjangoProject_1/urls.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('books.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]from django.contrib import admin
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
