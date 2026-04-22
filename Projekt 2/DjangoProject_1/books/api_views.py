40f5ab92b831e6ecb6c583c83c6c34d706699d37
kowalczykadam <60419569+kowalczykadam@users.noreply.github.com>
2026-04-22 15:44:44 +0200

    Update api_views.py

--- Projekt 2/DjangoProject_1/books/api_views.py
+++ Projekt 2/DjangoProject_1/books/api_views.py
@@ -1,12 +1,20 @@
-from rest_framework import viewsets, filters
+from rest_framework import viewsets, filters, permissions
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Book
 from .serializers import BookSerializer
+from .permissions import IsOwner
 
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated, IsOwner]
+
+    def get_queryset(self):
+        return Book.objects.filter(owner=self.request.user)
+    
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
     # filtrowanie po wielu polach
     filter_backends = [
